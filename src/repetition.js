076e744f83a023741d701504486d77a96ceb0020
Adam Sołtysik <adam27.sol@gmail.com>
2024-01-31 22:24:25 +0100

    Optimize repetition generator

--- src/repetition.js
+++ src/repetition.js
@@ -2,27 +2,67 @@
 import { Chess } from 'chess.js';
 
 
+const cache = {};
+let moves_tried;  // For tests
+
 export function generateRepetition(chess) {
     const fen = chess.fen().split(' ').slice(0, 4).join(' ');
+    moves_tried = 0;
 
-    for (const move1 of chess.moves({ verbose: true })) {
-        if (move1.piece === 'p') {
+    if (fen in cache) {
+        return cache[fen];
+    }
+    const store = result => {
+        cache[fen] = result;
+        return result;
+    }
+
+    const first_moves = chess.moves({ verbose: true });
+    if (first_moves.some(move => move.flags.includes('e'))) {
+        // Repetition not possible when en passant is available.
+        return store(null);
+    }
+
+    const isViable = move => {
+        // A pawn move or capture.
+        if (move.piece === 'p' || move.flags.includes('c')) {
+            return false;
+        }
+        // A rook or king move when there are still castling rights.
+        const castling = chess.getCastlingRights(chess.turn());
+        if (castling.k && (move.piece === 'r' && move.lan[0] === 'h' || move.piece === 'k')) {
+            return false;
+        }
+        if (castling.q && (move.piece === 'r' && move.lan[0] === 'a' || move.piece === 'k')) {
+            return false;
+        }
+        return true;
+    };
+    const makeMove = move => {
+        moves_tried += 1;
+        return chess.move(move);
+    };
+
+    for (const move1 of first_moves) {
+        if (!isViable(move1)) {
             continue;
         }
-        chess.move(move1.san);
+        makeMove(move1);
 
         for (const move2 of chess.moves({ verbose: true })) {
-            if (move2.piece === 'p') {
+            if (!isViable(move2)) {
                 continue;
             }
-            chess.move(move2.san);
+            makeMove(move2);
 
             try {
-                const move3 = chess.move({ from: move1.to, to: move1.from });
+                const move3 = makeMove({ from: move1.to, to: move1.from });
                 try {
-                    const move4 = chess.move({ from: move2.to, to: move2.from });
+                    const move4 = makeMove({ from: move2.to, to: move2.from });
                     if (chess.fen().split(' ').slice(0, 4).join(' ') === fen) {
-                        return [move1, move2, move3, move4].map(move => move.lan);
+                        return store([move1, move2, move3, move4].map(move => move.lan));
+                    } else {
+                        console.warn("generateRepetition: FEN mismatch")
                     }
                     chess.undo();
                 } catch {}
@@ -34,16 +74,31 @@ export function generateRepetition(chess) {
         }
         chess.undo();
     }
-    return null;
+    return store(null);
 }
 
 if (import.meta.vitest) {
     test('generateRepetition', () => {
         expect(generateRepetition(new Chess())).toEqual(['b1a3', 'b8c6', 'a3b1', 'c6b8']);
-        expect(generateRepetition(new Chess('7k/8/8/8/8/8/6r1/7K w'))).toBe(null);  // Capture necessary
-        expect(generateRepetition(new Chess('4k3/4P3/4K3/7p/8/8/8/8 w'))).toBe(null);  // Pawn move necessary
+        expect(moves_tried).toEqual(4);
+
+        expect(generateRepetition(new Chess('7k/8/8/8/8/8/6r1/7K w'))).toBe(null);
+        expect(moves_tried).toEqual(0);
+
+        expect(generateRepetition(new Chess('4k3/4P3/4K3/7p/8/8/8/8 w'))).toBe(null);
+        expect(moves_tried).toEqual(21);
+
         expect(generateRepetition(new Chess('r3k2r/3pPp2/3P1P2/8/8/3p1p2/3PpP2/R3K2R b Kq'))).toEqual(['h8h7', 'a1a2', 'h7h8', 'a2a1']);
-        expect(generateRepetition(new Chess('r3k2r/3pPp2/3P1P2/8/8/3p1p2/3PpP2/R3K2R b KQ'))).toBe(null);  // Losing castling rights necessary
+        expect(moves_tried).toEqual(4);
+
+        expect(generateRepetition(new Chess('r3k2r/3p1p2/3P1P2/8/8/3p1p2/3P1P2/R3K2R b KQ'))).toBe(null);
+        expect(moves_tried).toEqual(19);
+
+        expect(generateRepetition(new Chess('5k2/8/8/8/8/4n2q/8/5K2 w'))).toEqual(['f1e2', 'e3g2', 'e2f1', 'g2e3']);
+        expect(moves_tried).toEqual(22);
+
+        expect(generateRepetition(new Chess('K4k2/8/8/8/8/8/8/7b w'))).toBe(null);
+        expect(moves_tried).toEqual(52);
 
         const chess = new Chess();
         chess.move('e4');
@@ -54,6 +109,7 @@ if (import.meta.vitest) {
         chess.move('e5');
         chess.move('d5');
         expect(chess.fen()).toContain('d6');
-        expect(generateRepetition(chess)).toBe(null);  // En passant possible only for this turn
+        expect(generateRepetition(chess)).toBe(null);
+        expect(moves_tried).toEqual(0);
     });
 }
